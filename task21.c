fa8e265f51104955ac1778c23b0d74a242ea275c
vlad <vladichek3322@gmai.com>
2022-05-05 17:22:30 +0700

    task 21 done

--- task21.c
+++ task21.c
@@ -1,30 +1,104 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
+#include <stdlib.h>
 
-#define __USE_POSIX199309 1
+// от кого приходит ctrl c
+//почему нельзя использовать perror и printf
 
-static struct sigaction act;
+static int cnt = 0;
 
-void action(int a) {
-    printf("add");
+char int_to_char(int num) {
+    return num + '0';
 }
 
-int main(int argc, char **argv) {
-    pid_t cpid;
-    int status;
+int num_len(int num) {
+    int len = 0;
+    while (num != 0) {
+        len++;
+        num /= 10;
+    }
+    return len;
+}
 
-    sigset_t sigset;
-    act.sa_handler = action;
+void int_to_str(int num, char *buf, int len) {
+    char *p = buf + len;
+
+    *p = '\0';
+    p--;
+
+    while (num != 0) {
+       *p = int_to_char(num % 10);
+       num /= 10;
+       p--;
+    }
+}
+
+void quit() {
+    int len = num_len(cnt);
+    char buf[len + 1];
+    int_to_str(cnt, buf, len);
+
+    write(2, "\n", 1);
+    write(2, buf, len + 1);
+    write(2, "\n", 1);
+
+    exit(2);
+}
+
+void print() {
+    write(2, "\a", 1);
+    cnt++;
+}
+
+_Noreturn void action() {
+    struct sigaction act2, act3;
+    sigset_t sigset1;
+
+    sigemptyset(&sigset1);
+    sigaddset(&sigset1, SIGINT);
+
+    act2.sa_handler = quit;
+    act3.sa_handler = print;
+
+    sigprocmask(SIG_UNBLOCK, &sigset1, NULL);
+
+    if (sigaction(SIGQUIT, &act2, NULL) == -1){
+        write(2,"sigaction error\n", 17);
+    }
+
+    if (sigaction(SIGINT, &act3, NULL) == -1){
+        write(2,"sigaction error\n", 17);
+    }
+
+    write(2, "\nalarm!\n", 8);
+
+    print();
+
+    while (1) {
+        pause();
+    }
+}
+
+int main() {
+    struct sigaction act1;
+
+    char buf[3];
+    int_to_str(10, buf, 2);
 
 
-    //sigfillset(&act.sa_mask);
+    act1.sa_handler = action;
 
-    sigaction(SIGINT, &act, NULL);
+    sigfillset(&act1.sa_mask);
+    sigdelset(&act1.sa_mask, SIGQUIT);
 
-    raise(SIGINT);
+    if (sigaction(SIGINT, &act1, NULL) == -1) {
+        write(2,"sigaction error\n", 17);
+    }
 
-    //cpid = fork();
+    write(2, "sleeping\n", 9);
+    pause();
+
     return 0;
 }
 
---
+++ task25.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include <sys/wait.h>
+
+int main(int argc, char **argv) {
+
+
+    pipe()
+
+    return 0;
+}
+
+
+
